e821a07b865fcefb01a2f18999cccd2eec042298
AdamZed <adamzanon@hotmail.com>
2021-12-01 20:13:59 -0500

    cleanup and remove input

--- day01/day1.dyalog
+++ day01/day1.dyalog
@@ -1,5 +1,15 @@
+⍝ input is preloaded
+
+⍝ my first attempts
 Part1←{+/1↓⍵>¯1⌽⍵}
-Part2←{¯2↓(⍵)+(1⌽⍵)+(2⌽⍵)}
+Part2←Part1 {¯2↓(⍵)+(1⌽⍵)+(2⌽⍵)}
+
+⍝ my first attempt at making (at least part1) tacit
+Part1←+/1↓⊢>¯1∘⌽
+
+⍝ and now I have found out about using num,func,reduce for sliding window
+Part1←+/2</⊢
+Part2←Part1 3+/⊢
 
 'Part 1: ', Part1 input
-'Part 2: ', Part1 Part2 input
+'Part 2: ', Part2 input
--- day01/day1.py
+++ day01/day1.py
@@ -1,14 +1,12 @@
-import math
-
 def part1(nums):
-    return sum(1 for e in zip(nums, [math.inf] + nums[:-1]) if e[0] > e[1])
+    return sum(1 for l, r in zip(nums, nums[1:]) if l < r)
 
 def part2(nums):
     return part1([nums[i]+nums[i+1]+nums[i+2] for i in range(len(nums)-2)])
 
 def main():
     with open('input.txt') as f:
-        nums = [int(n) for n in f.readlines() if len(n.strip())>0]
+        nums = [int(n) for n in f.readlines() if len(n.strip()) > 0]
 
     print(f'Part 1: {part1(nums)}')
     print(f'Part 2: {part2(nums)}')
